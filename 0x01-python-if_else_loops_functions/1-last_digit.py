71dfe90e7268d349b16592ad6eff5dbe506647c6
MACRODAT <macrodatservices@gmail.com>
2023-10-31 14:38:21 +0100

    Changes to the file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    lastdigit = number % -10
+    digit_laster = number % -10
 else:
-    lastdigit = number % 10
-if lastdigit > 5:
+    digit_laster = number % 10
+if digit_laster > 5:
     print("Last digit of {:d} is {:d} and is greater than 5"
-          .format(number, lastdigit))
-elif lastdigit < 6 and lastdigit != 0:
+          .format(number, digit_laster))
+elif digit_laster < 6 and digit_laster != 0:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0"
-          .format(number, lastdigit))
+          .format(number, digit_laster))
 else:
     print("Last digit of {:d} is 0 and is 0".format(number))
