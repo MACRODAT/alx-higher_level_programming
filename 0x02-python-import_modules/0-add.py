55d0a2ef16ad55c48c1d9d33fa742c0c86725d25
MACRODAT <macrodatservices@gmail.com>
2023-11-03 01:14:27 +0100

    Changes to the file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-from "add_0.py" import add
-print(f"1 + 2 = {add(1,2)}")
+if __name__ == "__main__":
+    from "add_0.py" import add
+
+    a = 1
+    b = 1
+    print(f"1 + 2 = {add(1,2)}")
