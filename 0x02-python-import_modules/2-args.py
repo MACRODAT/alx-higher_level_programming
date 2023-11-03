7c5915a264cd9ea314a3691b3bebdd0f2c6d5d9d
MACRODAT <macrodatservices@gmail.com>
2023-11-03 01:54:11 +0100

    Changes to the file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     if n == 0:
         print("0 arguments.")
     else:
-        print(f"{n} argument{('s' if (n > 1) else '')}:")
+        print("{:d} argument{:s}:".format(n, ('s' if (n > 1) else '')))
         for i in range(1, n + 1):
-            print(f"{i}: {sys.argv[i]}")
+            print("{:d}: {:s}".format(i, sys.argv[i]))
