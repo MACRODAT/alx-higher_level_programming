529cf17913470dec5a1d851f607ea98cea393f12
MACRODAT <macrodatservices@gmail.com>
2023-11-03 01:48:39 +0100

    Changes to the file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     else:
         print(f"{n} argument{('s' if (n > 1) else '')}:")
         for i in range(n):
-            print(f"{i}: {sys.argv[i]}")
+            print(f"{i + 1}: {sys.argv[i]}")
