5c77f970bc0fe28ecab0f8af4ac7488d1b746012
MACRODAT <macrodatservices@gmail.com>
2023-11-03 01:58:40 +0100

    Changes to the file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
+    """z."""
     import sys
 
     n = len(sys.argv) - 1
     if n == 0:
         print("0 arguments.")
+    elif n == 1:
+        print("1 argument:")
     else:
-        print("{} argument{}:".format(n, ('s' if (n > 1) else '')))
-        for i in range(1, n + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments:".format(n))
+    for i in range(1, n + 1):
+        print("{}: {}".format(i, sys.argv[i]))
