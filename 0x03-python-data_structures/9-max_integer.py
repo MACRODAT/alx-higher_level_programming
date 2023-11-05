e00fbb6e76b5839560286b51ef231cc446721d20
MACRODAT <macrodatservices@gmail.com>
2023-11-05 17:52:51 +0100

    Changes to the file

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
     else:
-        max_int = my_list[0]
+        _max = my_list[0]
         for i in range(len(my_list)):
-            if my_list[i] > max_int:
-                max_int = my_list[i]
-        return (max_int)
+            if my_list[i] > _max:
+                _max = my_list[i]
+        return (_max)
