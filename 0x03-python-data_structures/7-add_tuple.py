2822d30f13d87260da7a08d3a60585ba7ea0e5d0
MACRODAT <macrodatservices@gmail.com>
2023-11-05 17:51:03 +0100

    Changes to the file

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,5 +10,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_a = 0, 0
         else:
             tuple_a = tuple_a[0], 0
-    added_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    return added_tuple
+    result = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return result
