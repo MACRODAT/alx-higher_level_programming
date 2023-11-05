93bbe606cf37cd4da78a499309220781123289b0
MACRODAT <macrodatservices@gmail.com>
2023-11-05 17:54:36 +0100

    Changes to the file

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    new_list = []
-
+    lst = []
     for i in my_list:
-        if i % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-    return (new_list)
+        lst += i % 2
+    return lst
