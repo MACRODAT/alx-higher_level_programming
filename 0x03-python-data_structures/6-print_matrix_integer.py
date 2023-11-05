25a27ce8b76675a7fa1e9d01d7de5a34a83d85b3
MACRODAT <macrodatservices@gmail.com>
2023-11-05 17:49:28 +0100

    Changes to the file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for column in row:
-            if column == row[-1]:
-                print('{:d}'.format(column), end='')
+    for rr_ow in matrix:
+        for _col in r_:
+            if _col == r_[-1]:
+                print('{:d}'.format(_col), end='')
             else:
-                print('{:d}'.format(column), end=' ')
+                print('{:d}'.format(_col), end=' ')
         print()
