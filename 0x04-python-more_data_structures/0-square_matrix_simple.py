e0c6d15421a64ad407a36c698158e759806ddb20
MACRODAT <macrodatservices@gmail.com>
2023-11-08 11:03:36 +0100

    Changes to the file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    nm = []
     for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            matrix[i][j] = matrix[i][j] * matrix[i][j]
+        nm.append([x ** 2 for x in matrix[i]])
+    return nm
