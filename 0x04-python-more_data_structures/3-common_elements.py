a16e1176878b216f7ecceb6b0ba908e61c40765a
MACRODAT <macrodatservices@gmail.com>
2023-11-08 11:24:45 +0100

    Changes to the file

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     s = set()
-    s.union(set1)
-    s.intersection(set2)
+    s = s.union(set_1)
+    s = s.intersection(set_2)
     return (s)
