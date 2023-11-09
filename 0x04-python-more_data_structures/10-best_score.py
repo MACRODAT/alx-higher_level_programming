68da73eb649138d95e4f9d47beb83d6f2854a4c9
MACRODAT <macrodatservices@gmail.com>
2023-11-09 02:16:25 +0100

    Changes to the file

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return (None)
     m_ = max(a_dictionary, key=lambda x: a_dictionary[x])
-    return m_
+    return (m_)
