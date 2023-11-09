bad5bcdca191e3508b1181e26f4bdd8f55178519
MACRODAT <macrodatservices@gmail.com>
2023-11-09 02:38:06 +0100

    Changes to the file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,14 @@
 
 
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if not roman_string:
         return 0
-    total = 0
-    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    for roman in reversed(roman_string):
-        arabic = digits[roman]
-        total += arabic if total < arabic * 5 else -arabic
-    return total
+    max_ = 0
+    nums = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    romanNumbers = list(map(lambda c: nums[c], roman_string))
+    for d in range(len(romanNumbers) - 1, -1, -1):
+        if romanNumbers[d] < max_:
+            romanNumbers[d] *= -1
+        else:
+            max_ = romanNumbers[d]
+    return sum(romanNumbers)
