46c18090f1c8d472513d890b540b862a8a50d7de
MACRODAT <macrodatservices@gmail.com>
2023-11-20 23:14:27 +0100

    Changes to the file

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,3 @@ def safe_print_integer_err(value):
     except (TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (False)
-safe_print_integer_err(4)
-safe_print_integer_err("2")
-safe_print_integer_err("he")
