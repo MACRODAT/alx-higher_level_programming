1a4eb20aa8409b33037d486b1f2d450c1831fdf4
MACRODAT <macrodatservices@gmail.com>
2023-11-20 23:23:12 +0100

    Changes to the file

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,8 +1,8 @@
 #include <Python.h>
 #include <stdio.h>
 /**
- * print_python_float - gives data of the PyFloatObject
- * @p: the PyObject
+ * print_python_float - e
+ * @p: ee
  */
 void print_python_float(PyObject *p)
 {
@@ -22,8 +22,8 @@ void print_python_float(PyObject *p)
 	printf("  value: %s\n", string);
 }
 /**
- * print_python_bytes - gives data of the PyBytesObject
- * @p: the PyObject
+ * print_python_bytes - e
+ * @p: te
  */
 void print_python_bytes(PyObject *p)
 {
@@ -48,35 +48,4 @@ void print_python_bytes(PyObject *p)
 		i++;
 	}
 	printf("\n");
-}
-/**
- * print_python_list - gives data of the PyListObject
- * @p: the PyObject
- */
-void print_python_list(PyObject *p)
-{
-	Py_ssize_t size = 0;
-	PyObject *item;
-	int i = 0;
-
-	fflush(stdout);
-	printf("[*] Python list info\n");
-	if (PyList_CheckExact(p))
-	{
-		size = PyList_GET_SIZE(p);
-		printf("[*] Size of the Python List = %zd\n", size);
-		printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-		while (i < size)
-		{
-			item = PyList_GET_ITEM(p, i);
-			printf("Element %d: %s\n", i, item->ob_type->tp_name);
-			if (PyBytes_Check(item))
-				print_python_bytes(item);
-			else if (PyFloat_Check(item))
-				print_python_float(item);
-			i++;
-		}
-	}
-	else
-		printf("  [ERROR] Invalid List Object\n");
 }
