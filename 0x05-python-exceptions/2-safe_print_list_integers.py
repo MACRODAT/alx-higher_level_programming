d2b4b55756e5060ace51e70326b659ede5a3ca48
MACRODAT <macrodatservices@gmail.com>
2023-11-20 23:04:27 +0100

    Changes to the file

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,16 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
+    p = 0
     while (i < x):
         try:
             print("{:d}".format(my_list[i]), end="")
+            p += 1
+        except (TypeError, ValueError):
+            pass
         except Exception as e:
             print()
-            return i
+            return p
         i += 1
     print()
-    return i
+    return p
