f41ee9ef043cbff452e29d73771db8dddec130eb
MACRODAT <macrodatservices@gmail.com>
2023-11-20 22:47:49 +0100

    Changes to the file

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except (RuntimeError, TypeError):
+    except Exception as e:
         return (False)
