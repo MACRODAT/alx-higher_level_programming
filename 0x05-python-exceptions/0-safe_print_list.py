93a25233649b33de0b5fe46dfbdd40bb49c91c8f
MACRODAT <macrodatservices@gmail.com>
2023-11-20 22:56:29 +0100

    Changes to the file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
         except Exception as e:
+            print()
             return i
         i += 1
+    print()
     return i
