32602fb6577b53a9db26d0a36b16ff7d8b16f86c
MACRODAT <macrodatservices@gmail.com>
2023-11-21 06:19:48 +0100

    Changes to the file

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
-
-
 class Square:
-    """Represent a square."""
-
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-            position (int, int): The position of the new square.
-        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -44,28 +32,25 @@ class Square:
         self.__position = value
 
     def area(self):
-        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
-        """Print the square with the # character."""
         if self.__size == 0:
-            print("")
+            print()
             return
 
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+            [print(" ", end="") for _ in range(0, self.__position[0])]
+            [print("#", end="") for _ in range(0, self.__size)]
+            print()
 
     def __str__(self):
-        """Define the print() representation of a Square."""
         if self.__size != 0:
             [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
+            [print(" ", end="") for _ in range(0, self.__position[0])]
+            [print("#", end="") for _ in range(0, self.__size)]
             if i != self.__size - 1:
-                print("")
+                print()
         return ("")
