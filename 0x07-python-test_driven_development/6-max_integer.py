c7b5b0f077dbebe80d234280eb5f5bc779851891
MACRODAT <macrodatservices@gmail.com>
2023-11-24 05:05:56 +0100

    Changes to the file

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -4,15 +4,15 @@
 
 
 def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
+    """find and return the max integer in a list of integers
         If the list is empty, the function returns None
     """
     if len(list) == 0:
         return None
-    result = list[0]
+    themax = list[0]
     i = 1
     while i < len(list):
-        if list[i] > result:
-            result = list[i]
+        if list[i] > themax:
+            themax = list[i]
         i += 1
-    return result
+    return themax
