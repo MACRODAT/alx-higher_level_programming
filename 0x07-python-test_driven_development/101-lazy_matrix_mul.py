394209affddc5a1cb64d05f35b9761fdf3f755e3
MACRODAT <macrodatservices@gmail.com>
2023-11-24 05:07:06 +0100

    Changes to the file

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a matrix multiplication function using NumPy."""
+"""using NumPy."""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Return the multiplication of two matrices.
+    """multiplication of 2 matrices.
 
     Args:
-        m_a (list of lists of ints/floats): The first matrix.
-        m_b (list of lists of ints/floats): The second matrix.
+        m_a (list of lists of ints/floats): The 1 matrix.
+        m_b (list of lists of ints/floats): The 2 matrix.
     """
 
     return (np.matmul(m_a, m_b))
