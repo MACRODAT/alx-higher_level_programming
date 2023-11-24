d4d439114f5411f0d5ec44138b9bc9d512355f92
MACRODAT <macrodatservices@gmail.com>
2023-11-24 05:05:05 +0100

    Changes to the file

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a text-indentation function."""
+"""text-indentation fct."""
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
+    """text with 2 new lines after '.', '?' ':'.
 
     Args:
-        text (string): The text to print.
+        text (string): text to print
     Raises:
-        TypeError: If text is not a string.
+        TypeError: text is not a string
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
