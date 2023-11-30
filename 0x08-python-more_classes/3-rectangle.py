3cf070c81a91a712a3af609035c8acbb2c1f99af
MACRODAT <macrodatservices@gmail.com>
2023-11-30 02:52:17 +0100

    Changes to the file

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -58,7 +58,6 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
+            [rect.append('#') for _ in range(self.__width)]
+        rect.append("\n")
         return ("".join(rect))
