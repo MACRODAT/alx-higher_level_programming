d69a2efaa7145d2e4511ad40d34abea9319de34e
MACRODAT <macrodatservices@gmail.com>
2023-12-04 23:28:20 +0100

    Changes to the file

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,7 +13,7 @@ class BaseGeometry:
         """checker fpr
         value
         """
-        if type(value) != int:
+        if type(value) is int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
