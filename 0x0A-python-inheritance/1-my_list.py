7c7f9daaeb0fcc5e196903a9f2986159c657f941
MACRODAT <macrodatservices@gmail.com>
2023-12-04 22:50:20 +0100

    Changes to the file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,8 +2,11 @@
 
 """ base help """
 
+
 class MyList(list):
 	def __init__(self):
+		""" help here """
 		super().__init__()
 	def print_sorted(self):
-		for e in sorted(self.)
+		""" print sorted """
+		return sorted(self)
