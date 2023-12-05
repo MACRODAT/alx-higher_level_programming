72ed3fa3404dae24b574f9d0537a347d688b742b
MACRODAT <macrodatservices@gmail.com>
2023-12-05 09:39:49 +0100

    Changes to the file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ def write_file(filename="", text=""):
     """ wr a file """
     if filename == "":
         return
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "w") as f:
         f.write(text)
