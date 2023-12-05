75c0849d84b3b1cb77930927cc7d709d1902d3f3
MACRODAT <macrodatservices@gmail.com>
2023-12-05 14:00:51 +0100

    Changes to the file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-""" add item """
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
-    sv = __import__("5-save_to_json_file").save_to_json_file
-    ld = __import__("6-load_from_json_file").load_from_json_file
-
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        data_lst = ld("add_item.json")
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        data_lst = []
-    data_lst.extend(sys.argv[1:])
-    sv(data_lst, "add_item.json")
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
+
