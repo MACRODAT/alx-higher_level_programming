ad4b197ed6d8db4d40dae7a565bf7a9676bfa0dd
MACRODAT <macrodatservices@gmail.com>
2023-12-05 02:57:19 +0100

    Changes to the file

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-"""Defines BaseGeometry."""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """something."""
+    """Reprsent base geometry."""
 
     def area(self):
-        """Not yet."""
+        """Not yet implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """checker fpr
-        value
+        """Validate a parameter as an integer.
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
         """
-        if type(value) is int:
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
