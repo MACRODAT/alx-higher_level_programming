af721cc1c81ec6f7930c6a832ab1ffdd1e1cb73d
MACRODAT <macrodatservices@gmail.com>
2023-12-05 13:45:53 +0100

    Changes to the file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-
 """ add item """
-
-
-
+import sys
 
 if __name__ == "__main__":
-    import sys
     sv = __import__("5-save_to_json_file").save_to_json_file
     ld = __import__("6-load_from_json_file").load_from_json_file
 
