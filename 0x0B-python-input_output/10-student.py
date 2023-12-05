16aba5887d08c0d722e86ecbc63f723cc8ed52ad
MACRODAT <macrodatservices@gmail.com>
2023-12-05 14:28:56 +0100

    Changes to the file

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""Student."""
 
 
 class Student:
-    """Represent a student."""
+    """a student."""
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -19,14 +14,8 @@ class Student:
 
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
-
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-
-        Args:
-            attrs (list): (Optional) The attributes to represent.
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        if (type(attrs) == list):
+            if (all(type(e) == str for e in attrs)):
+                return {att: getattr(self, att) for att in attrs if hasattr(self, att)}             
         return self.__dict__
