05b3d37e8bcca1baddfc280b651d3cc4ffcfacfd
MACRODAT <macrodatservices@gmail.com>
2023-12-07 21:35:19 +0100

    Changes to the file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@
     python file for storing data
 """
 
-Base = __import__("base.py").Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -12,11 +12,11 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ class initializer """
-        super(self, id)
-        self.width(width)
-        self.height(height)
-        self.x(x)
-        self.y(y)
+        super().__init__(id)
+        self.width = (width)
+        self.height = (height)
+        self.x = (x)
+        self.y = (y)
 
     @property
     def width(self):
