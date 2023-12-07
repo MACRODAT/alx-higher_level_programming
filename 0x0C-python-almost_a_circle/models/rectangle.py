6e07d6da5d36354ca183846823b6f985fd2ab817
MACRODAT <macrodatservices@gmail.com>
2023-12-07 21:22:32 +0100

    Changes to the file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -28,6 +28,8 @@ class Rectangle(Base):
         """set width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -40,6 +42,8 @@ class Rectangle(Base):
         """set height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -52,6 +56,8 @@ class Rectangle(Base):
         """set x"""
         if type(value) is not int:
             raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -64,4 +70,6 @@ class Rectangle(Base):
         """set y"""
         if type(value) is not int:
             raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
