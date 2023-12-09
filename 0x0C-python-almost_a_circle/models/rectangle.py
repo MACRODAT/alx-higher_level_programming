fe6612b4272cf253cf87bd778d9518b1ab58c39b
MACRODAT <macrodatservices@gmail.com>
2023-12-09 23:37:58 +0100

    Changes to the file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,8 +88,8 @@ class Rectangle(Base):
     def __str__(self) -> str:
         """ override """
         return \
-            f"[Rectangle] ({self.id}) {self.x}/{self.y}\
-                  - {self.width}/{self.height}"
+            f"[Rectangle] ({self.id}) {self.x}/{self.y} \
+- {self.width}/{self.height}"
 
     def update(self, *args):
         """ updates the rectangle """
