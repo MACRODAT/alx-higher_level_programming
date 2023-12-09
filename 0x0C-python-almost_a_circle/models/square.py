a2b911ea5a0aabd29831ae570347d2e6e62222e9
MACRODAT <macrodatservices@gmail.com>
2023-12-10 00:09:24 +0100

    Changes to the file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,3 +13,14 @@ class Square(Rectangle):
 
     def __str__(self) -> str:
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """ return the size of the square """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ set the size for value """
+        self.width = value
+        self.height = value
