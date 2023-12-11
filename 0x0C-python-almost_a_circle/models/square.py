9d49d2b74f2b54ee74997a3918a7ed58cb39e067
MACRODAT <macrodatservices@gmail.com>
2023-12-12 00:34:23 +0100

    Changes to the file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -45,8 +45,8 @@ class Square(Rectangle):
     def to_dictionary(self):
         """ dict repres """
         return {
-            "id" : self.id, 
-            "x" : self.x,
-            "size" : self.size,
-            "y" : self.y,
+            "id": self.id,
+            "x": self.x,
+            "size": self.size,
+            "y": self.y,
         }
