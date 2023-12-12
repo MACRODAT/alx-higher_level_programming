c525d763d10e98b996592e5766f8d27252ea29dd
MACRODAT <macrodatservices@gmail.com>
2023-12-12 02:32:35 +0100

    Changes to the file

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -4,6 +4,10 @@ import pep8
 import os
 import sys
 import io
+
+sys.path.append(os.path.dirname(sys.path[0]))
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "models"))
+
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
