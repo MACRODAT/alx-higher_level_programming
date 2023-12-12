94ae5ba13e1227caa70d284b59d1ce0834b0a0f1
MACRODAT <macrodatservices@gmail.com>
2023-12-12 03:08:56 +0100

    Changes to the file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,23 +1,32 @@
-#!/bin/python3
-'''
-    python file for storing data
-'''
+#!/usr/bin/python3
+"""Defines a base model class."""
 import json
+import csv
 import turtle
 
 
 class Base:
-    ''' base class '''
+    """Represent the base model.
+
+    Represents the "base" for all other classes in project 0x0C*.
+
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        '''simple def
-        '''
-        if id is None:
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        else:
-            self.id = id
 
     @staticmethod
     def to_json_string(list_dictionaries):
