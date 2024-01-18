ce63115842e00c361af0dfbe09dbe935a474dea9
MACRODAT <macrodatservices@gmail.com>
2024-01-18 03:07:23 +0100

    Changes to the file

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
--- Ld
-SELECT `id`, `name`
-  FROM `cities`
- INNER JOIN `states` ON `cities`.`state_id` = `states`.`id`
- WHERE `states`.`name` = 'California'
- ORDER BY `id`;
+-- Modified Script 1 to achieve the same result as Script 3
+SELECT `cities`.`id`, `cities`.`name`
+FROM `cities`
+INNER JOIN `states` ON `cities`.`state_id` = `states`.`id`
+WHERE `states`.`name` = 'California'
+ORDER BY `cities`.`id`;
