14b2a6c884edaf65d18a597c12661f6b32a156c1
MACRODAT <macrodatservices@gmail.com>
2024-02-13 03:51:29 +0100

    Changes to the file

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,10 +2,10 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  const x = Number(process.argv[2]);
+  const numb = Number(process.argv[2]);
   let i = 0;
-  while (i < x) {
-    console.log('X'.repeat(x));
+  while (i < numb) {
+    console.log('X'.repeat(numb));
     i++;
   }
 }
