af1862eabaa30b122deab3257df7b37a39a230b8
MACRODAT <macrodatservices@gmail.com>
2024-02-16 08:36:11 +0100

    Changes to the file

--- 0x0F-python-object-relational_mapping/0-select_states.py
+++ 0x0F-python-object-relational_mapping/0-select_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost", port=3306,
-            user = username, passwd=password, db=db_name)
+                         user=username, passwd=password, db=db_name)
     cur = db.cursor()
     r = cur.execute("SELECT * FROM states ORDER BY id ASC")
     for state in r:
