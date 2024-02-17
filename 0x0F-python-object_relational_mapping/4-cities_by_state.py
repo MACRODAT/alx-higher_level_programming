8b75e2a90d3fbe9d6d03416791334283f4f21bcb
MACRODAT <macrodatservices@gmail.com>
2024-02-17 01:50:42 +0100

    Changes to the file

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute(""" SELECT cities.id, cities.name, states.name
                 FROM cities LEFT JOIN states ON
-                cities.id = states.id
+                cities.state_id = states.id
                 ORDER BY cities.id ASC
     """)
     query_rows = cur.fetchall()
