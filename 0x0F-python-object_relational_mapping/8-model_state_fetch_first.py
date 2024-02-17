985f999c035a93b09f5a405277776015c3caa060
MACRODAT <macrodatservices@gmail.com>
2024-02-17 17:24:34 +0100

    Changes to the file

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(engine)
     session = Session()
-    for state in session.query(State).order_by(State.id):
-        print(f"{state.id} : {state.name}")
+    state = session.query(State).order_by(State.id).first()
+    print(f"{state.id} : {state.name}")
     session.close()
