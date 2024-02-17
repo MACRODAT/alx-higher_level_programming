8f22de4024d9e5a3052c7c9ff7e594ccdd8030f5
MACRODAT <macrodatservices@gmail.com>
2024-02-17 01:46:32 +0100

    Changes to the file

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+lists all cities
+"""
+if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                            passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute(""" SELECT cities.id, cities.name, states.name
+                FROM cities INNER JOIN states ON
+                cities.id = states.id
+                ORDER BY cities.id ASC
+    """)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
