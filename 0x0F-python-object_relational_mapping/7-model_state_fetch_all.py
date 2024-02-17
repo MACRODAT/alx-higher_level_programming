f09268968ee585f88ee10a239f20d0165bb0c77a
MACRODAT <macrodatservices@gmail.com>
2024-02-17 15:43:36 +0100

    Changes to the file

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+    fetch all states
+"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine(url=f"{argv[1]}@{argv[2]}localhost:3306//{argv[3]}")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        print(f"{state.id} : {state.name}")
+    session.close()
