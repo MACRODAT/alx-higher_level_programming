4891569e34519b5e10332cd32ced31db73bfefe3
MACRODAT <macrodatservices@gmail.com>
2024-02-17 15:29:43 +0100

    Changes to the file

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+    class definition
+"""
+
+from sqlalchemy import Column, Integer, Text
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+        table state
+    """
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, nullable=False, auto_increment=True)
+    name = Column(Text(128), nullable=False)
