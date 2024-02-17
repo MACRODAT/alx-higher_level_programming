be288b152b3b15a92038069698fd43f925b53396
MACRODAT <macrodatservices@gmail.com>
2024-02-17 01:14:35 +0100

    Changes to the file

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    script to get all states
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=db_name)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states
+                WHERE name==`N*`
+                ORDER BY id ASC""")
+    r = cur.fetchall()
+    for state in r:
+        print(state)
