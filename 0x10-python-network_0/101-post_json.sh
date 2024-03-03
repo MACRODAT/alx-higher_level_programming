78b71a329bbc219c91fa782f3b0b6f6c3aba892f
MACRODAT <macrodatservices@gmail.com>
2024-03-03 15:00:58 +0100

    Changes to the file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-# sends a JSON POST request to a URL, and displays the body of the response.
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+# sends a POST req
+# curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+# Color code variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+NC='\033[0m' # No Color
+
+if [ $# -ne 1]; then
+	echo "${RED}Usage: $0 <URL:PORT>${NC}"
+	exit 1
+fi
+
+echo -e "${BLUE}Starting request...${NC}";
+URL=$1
