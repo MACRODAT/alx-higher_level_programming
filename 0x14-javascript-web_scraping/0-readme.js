334df58f152bbc3043d8ee3dc5f84cd6abb822d6
MACRODAT <macrodatservices@gmail.com>
2024-03-27 02:45:25 +0000

    Changes to the file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,16 @@
+#!/usr/bin/node
+// A script read files
+
 const fs = require('fs');
 
-function readfs(fp) {
-	fs.readFile(fp, 'utf-8', (err, data) => {
-		if (err) {
-			console.error(err);
-		}
-		else {
-			console.log(data);
-		}
-	});
+function readfs (fp) {
+  fs.readFile(fp, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(data);
+    }
+  });
 }
 
-readfs(process.argv[2])
+readfs(process.argv[2]);
