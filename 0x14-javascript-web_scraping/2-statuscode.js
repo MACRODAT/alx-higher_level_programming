d1084e26cb5fe4a3fe0b697517945e62cdbf8e6e
MACRODAT <macrodatservices@gmail.com>
2024-03-27 02:52:00 +0000

    Changes to the file

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// get req status
+
+const url_ = process.argv[2];
+const request = require('request');
+request(url_, function (error, response, body) {
+  if (error) {
+    console.log('error:', error); // Print the error if one occurred
+  } else {
+    console.log('code:', response && response.statusCode);
+  }
+});
