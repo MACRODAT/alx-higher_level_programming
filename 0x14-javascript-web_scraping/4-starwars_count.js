043989aef73b1cc0ccea849e8db329f6ad58f9f9
MACRODAT <macrodatservices@gmail.com>
2024-03-27 03:02:39 +0000

    Changes to the file

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-// A script that prints the number of movies where the character Wedge Antilles is present
+// number of movies
 
 const args = process.argv;
 let reqURL = args[2];
 let request = require('request');
-request(reqURL, function (error, response, body) {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  } else {
+request(reqURL, function (err, response, body) {
+  if (err) {
+    console.error(err);
     let jso = JSON.parse(body);
     let results = jso['results'];
     let count = 0;
